e8d2687fa01d7d60027e62309d4678c4febc9687
L-yo <eliott.duverger@hotmail.fr>
2020-10-09 11:54:45 +0200

    openface exclusive

--- face.py
+++ face.py
@@ -1,27 +1,42 @@
-from torch import nn, optim, as_tensor
-from torch.utils.data import Dataset, DataLoader
-import torch.nn.functional as F
-from torch.optim import lr_scheduler
-from torch.nn.init import *
-from torchvision import transforms, utils, datasets, models
-from models.inception_resnet_v1 import InceptionResnetV1
-
-import cv2
-from PIL import Image
-from pdb import set_trace
-import time
-import copy
-
-from pathlib import Path
-import os
 import sys
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
-from skimage import io, transform
-from tqdm import trange, tqdm
-import csv
-import glob
-#import dlib
+import dlib
+import cv2
+from openface import openface
 
-import pandas as pd
-import numpy as np
+# http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2
+predictor_model = "shape_predictor_68_face_landmarks.dat"
+
+# Create a HOG face detector using the built-in dlib class
+face_detector = dlib.get_frontal_face_detector()
+face_pose_predictor = dlib.shape_predictor(predictor_model)
+face_aligner = openface.AlignDlib(predictor_model)
+
+# Take the image file name from the command line
+file_name = "img5.jpg"
+file_path = "img/"
+
+FILE = file_path + file_name
+# Load the image
+image = cv2.imread(FILE)
+
+# Run the HOG face detector on the image data
+detected_faces = face_detector(image, 1)
+
+print("Found {} faces in the image file {}".format(len(detected_faces), FILE))
+
+# Loop through each face we found in the image
+for i, face_rect in enumerate(detected_faces):
+
+	# Detected faces are returned as an object with the coordinates
+	# of the top, left, right and bottom edges
+	print("- Face #{} found at Left: {} Top: {} Right: {} Bottom: {}".format(i, face_rect.left(), face_rect.top(), face_rect.right(), face_rect.bottom()))
+
+	# Get the the face's pose
+	pose_landmarks = face_pose_predictor(image, face_rect)
+
+	# Use openface to calculate and perform the face alignment
+	alignedFace = face_aligner.align(
+		534, image, face_rect, landmarkIndices=openface.AlignDlib.OUTER_EYES_AND_NOSE)
+
+	# Save the aligned image to a file
+	cv2.imwrite("aligned_faces/" +  file_name + "_{}.jpg".format(i), alignedFace)
